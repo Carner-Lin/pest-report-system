0f89da0313e68b5bf059c2fbe55183a1d8862741
Ziqi Lin <zl354@students.waikato.ac.nz>
2026-05-15 00:10:24 +1200

    Modify db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
-// backend/db.js
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1674249",
-    database: "zl354"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
 });
 
 db.connect(err => {
